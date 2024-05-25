caac7dc6cab9a0a203d1a52464f5482d58338372
Monalicous <samukelisiwentusi@outlook.com>
2024-05-25 09:26:14 +0400

    JS file updates

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,1 +1,10 @@
+const destinations = document.getElementById('destionations');
+const searchBtn = document.getElementById('searchBtn');
 
+searchBtn.addEventListener('click'), function(){
+    const searchCountries = searchInput.value;
+}
+
+document.getElementById("resetBtn").onclick = function(){
+    document.getElementById("destinations").innerHTML="";
+}
