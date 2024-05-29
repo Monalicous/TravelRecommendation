d4f8d0ae8d807f0cdca56b8e0c77dc8addeefa9e
Monalicous <samukelisiwentusi@outlook.com>
2024-05-29 12:22:36 +0400

    Updated files for Travel Recommendation Website project

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,12 +1,17 @@
+const travelRecommendation = document.getElementById('travelRecommendation');
+const recommendation =document.getElementById('recommendation');
+const searchBtn = document.getElementById('searchBtn');
+const vacation = [];
 
 const options = { timeZone: 'America/New_York', hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
 const newYorkTime = new Date().toLocaleTimeString('en-US', options);
 console.log("Current time in New York:", newYorkTime);
 
+
 function searchRecommendations(){
     const input = document.getElementById('travelRecommendation').value.toLowerCase();
-    const recommendationLi = document.getElementById('recommendation');
-    recommendationLi.innerHTML = '';
+    const recommendationDiv = document.getElementById('recommendation');
+    recommendationDiv.innerHTML= '';
 
     fetch ('travel_recommendation.api.json')
     .then(response => response.json())
@@ -21,21 +26,21 @@ function searchRecommendations(){
             const cities = country.cities.join(',');
             const description = country.description.join(',');
             
-
-            input.innerHTML += `<img src = "${country.imagesrc}" alt="hjh">` ;
             input.innerHTML += `${country.name}` || `${beach.name}` || `${temple.name}`;
+            input.innerHTML += `<img src = "${country.imagesrc}" alt="hjh">` ;
 
             input.innerHTML += `<p><strong>Cities:</strong> ${cities}</p>`;
-            recommendationLi.innerHTML += `<p><strong>Description:</strong> ${description}</p>`;
+            recommendationDiv.innerHTML += `<p><strong>Description:</strong> ${description}</p>`;
             } else {
-              recommendationLi.innerHTML = 'Country could not found.';
+              recommendationDiv.innerHTML = 'Country could not be found.';
             }
           })
 
           .catch(error => {
             console.log=('Error:', error);
-            recommendationLi.innerHTML = 'An error occurred while fetching data.';
+            recommendationDiv.innerHTML = 'An error occurred while fetching data.';
         });
 }
 
 searchBtn.addEventListener('click', searchRecommendations);
+
